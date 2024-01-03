02e475f1ffb4e1777124eba4a04ddde7c1825905
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2024-01-03 11:08:26 +0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
     #[cfg(target_os = "linux")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 
-    println!("cargo:rustc-link-lib=png");
+    // println!("cargo:rustc-link-lib=png");
 
     // println!("cargo:rerun-if-env-changed=SDL2_TTF_LIBRARY_PATH");
     // if let Ok(library_path) = std::env::var("SDL2_TTF_LIBRARY_PATH") {
